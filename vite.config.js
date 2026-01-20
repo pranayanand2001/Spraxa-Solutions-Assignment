48a7b1fcd07a3d0a45b92762b1d4dd1270bfdcdc
Pranay <pranayanand.sm@gmail.com>
2026-01-20 21:18:55 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/spraxa-solutions-assignment/",
+   base: "/Spraxa-Solutions-Assignment/",
   plugins: [react()],
 })
